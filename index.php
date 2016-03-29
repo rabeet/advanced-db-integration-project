9fa07bd2ce699b2ad9105926c0d629e5204bdbeb
Rabeet Fatmi <raf6000@gmail.com>
2016-03-29 08:41:54 +0000

    css

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ redirect_if_logged_in(HTTP_SCRIPT_HOME."/courses.php");
 <html>
 <head>
 	<title>LMS Login</title>
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="/stylesheets/main.css" />
 </head>
 <body>
 	<h1>LMS Login</h1><br>
