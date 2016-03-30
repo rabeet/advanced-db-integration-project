53cf8ecd72847340348efc7cb2115b6b7b6f1fb7
Rabeet Fatmi <raf6000@gmail.com>
2016-03-30 01:35:43 +0000

    still doing riak

--- riak.php
+++ riak.php
@@ -1,11 +1,11 @@
 <?php
 
-// require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// // lib classes are included via the Composer autoloader files
-// use Basho\Riak;
-// use Basho\Riak\Node;
-// use Basho\Riak\Command;
+// lib classes are included via the Composer autoloader files
+use Basho\Riak;
+use Basho\Riak\Node;
+use Basho\Riak\Command;
 
 // define the connection info to our Riak nodes
 $port = getenv('PORT');
