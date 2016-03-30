90381f6bda7c11669f366c5f5e3cd02b8b60adda
Rabeet Fatmi <raf6000@gmail.com>
2016-03-30 09:20:53 +0000

    create course edits

--- courses/create.php
+++ courses/create.php
@@ -5,6 +5,6 @@ require_professor();
 if(!isset($_POST["coursename"]) || !isset($_POST["semester_year"]) || !isset($_POST["semester_term"])) {
     error("Please check the submitted info.");
 } else {
-    createCourse();
+    // createCourse();
 }
 ?>
