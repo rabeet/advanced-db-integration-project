ac0894cbb50be38cf8661237de5b27f1d1ae5c0d
Rabeet Fatmi <raf6000@gmail.com>
2016-04-27 06:32:56 +0000

    editing configs

--- config.php
+++ config.php
@@ -6,10 +6,10 @@ define("HTTP_SCRIPT_HOME", "/");
 $ROLE["PROF"] = "Professor";
 $ROLE["STU"] = "Student";
 
-$POSTGRES["user"] = "jdbcavzihhfobw";
-$POSTGRES["password"] = "xZSFZAUuOReDRZ8IeRM6K1vV6n";
-$POSTGRES["host"] = "ec2-54-235-246-67.compute-1.amazonaws.com";
-$POSTGRES["dbname"] = "dn897bdaulq6o";
+$POSTGRES["user"] = "rdmsrrqhbocofm";
+$POSTGRES["password"] = "rbElCXko0lUGSwS1thjP9IpgKZ";
+$POSTGRES["host"] = "ec2-54-163-226-48.compute-1.amazonaws.com";
+$POSTGRES["dbname"] = "dfnl87c9mh4cco";
 $POSTGRES["port"] = "5432";
 $POSTGRES["string"] = "host=".$POSTGRES["host"]." port=".$POSTGRES["port"]." dbname=".$POSTGRES["dbname"]." user=".$POSTGRES["user"]." password=".$POSTGRES["password"];
 
